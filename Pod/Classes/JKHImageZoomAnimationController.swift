927929fe1379888ac1f92070767e3d7a3a5aba96
Joon Ki Hong <jhong70@icloud.com>
2016-12-12 15:27:10 -0500

    Pod Classes weren't updated

--- Pod/Classes/JKHImageZoomAnimationController.swift
+++ Pod/Classes/JKHImageZoomAnimationController.swift
@@ -28,7 +28,7 @@ class JKHImageZoomAnimationController: NSObject, UIViewControllerAnimatedTransit
     var springDamping: CGFloat
     var type: JKHImageZoomType
     
-    init(type: JKHImageZoomType, backgroundColor: UIColor = UIColor.white, viewContentMode: UIViewContentMode = .scaleAspectFill, duration: TimeInterval = 0.5, springVelocity: CGFloat = 1.5, springDamping: CGFloat = 1.0) {
+    init(type: JKHImageZoomType, backgroundColor: UIColor = UIColor.white, viewContentMode: UIViewContentMode = .scaleAspectFill, duration: TimeInterval = 0.7, springVelocity: CGFloat = 1.5, springDamping: CGFloat = 1.0) {
         self.backgroundColor = backgroundColor
         self.viewContentMode = viewContentMode
         self.duration = duration
@@ -68,15 +68,10 @@ class JKHImageZoomAnimationController: NSObject, UIViewControllerAnimatedTransit
             toVC = (toVC as! UITabBarController).selectedViewController!
         }
         
-        guard let fromVCSnapshot = fromView.snapshotView(afterScreenUpdates: false) else {
-            return
-        }
-        
         containerView.backgroundColor = backgroundColor
         containerView.addSubview(toView)
-        containerView.addSubview(fromVCSnapshot)
+        containerView.addSubview(fromView)
         
-        fromView.isHidden = true
         toView.setNeedsLayout()
         toView.layoutIfNeeded()
         
@@ -91,37 +86,45 @@ class JKHImageZoomAnimationController: NSObject, UIViewControllerAnimatedTransit
         
         containerView.addSubview(fromImageViewCopy)
         
-        fromVCSnapshot.layer.anchorPoint = CGPoint(x: fromImageViewCopy.center.x/containerView.frame.size.width, y: fromImageViewCopy.center.y/containerView.frame.size.height)
-        fromVCSnapshot.layer.position = fromImageViewCopy.center
-
-//        toView.layer.anchorPoint = CGPointMake(toImageView.center.x/containerView.frame.size.width, toImageView.center.y/containerView.frame.size.height)
-//        toView.layer.position = toImageView.center
-    
-        let transforms = transformsForZoom(type, toFrame: toImageViewFrame, fromFrame: fromImageViewCopy.frame)
+        let fromAnchorPoint = CGPoint(x: fromImageViewCopy.center.x/containerView.frame.size.width, y: fromImageViewCopy.center.y/containerView.frame.size.height)
+        let toAnchorPoint = CGPoint(x: toImageViewFrame.midX/containerView.frame.size.width, y: toImageViewFrame.midY/containerView.frame.size.height)
+        let transforms = transformsForZoom(withType: type, toFrame: toImageViewFrame, fromFrame: fromImageViewCopy.frame)
+        
+        setAnchorPoint(fromAnchorPoint, forView: fromView)
+        setAnchorPoint(toAnchorPoint, forView: toView)
         
         toView.transform = transforms.to
-
-        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, usingSpringWithDamping: springDamping, initialSpringVelocity: springVelocity, options: .curveLinear, animations: {
+        
+        self.animate(withTransitionContext: transitionContext, animations: {
             fromImageViewCopy.frame = toImageViewFrame
-            fromVCSnapshot.alpha = 0
-            fromVCSnapshot.transform = transforms.from
-            toView.transform = CGAffineTransform.identity
-            tabBar?.transform = CGAffineTransform.identity
+            fromView.alpha = 0
+            fromView.transform = transforms.from
+            toView.transform = .identity
         }) { (completed) in
-            if completed {
-                toView.layer.position = toView.center
-                fromView.isHidden = false
-                fromImageViewCopy.removeFromSuperview()
-                fromVCSnapshot.removeFromSuperview()
-                (toVC as! JKHImageZoomTransitionProtocol).transitionDidFinish?(true, finalImage: fromImageViewCopy.image!)
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            } else {
-                (toVC as! JKHImageZoomTransitionProtocol).transitionDidFinish?(false, finalImage: fromImageViewCopy.image!)
+            toView.layer.position = toView.center
+            fromView.alpha = 1
+            fromView.transform = .identity
+            fromImageViewCopy.removeFromSuperview()
+            fromView.removeFromSuperview()
+            
+            if tabBar != nil { // Animate the tab bar after animation
+                self.animate(withTransitionContext: transitionContext, animations: {
+                    tabBar?.transform = CGAffineTransform.identity
+                }, completion: nil)
             }
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            
+            (toVC as! JKHImageZoomTransitionProtocol).transitionDidFinish?(true, finalImage: fromImageViewCopy.image!)
         }
     }
     
-    fileprivate func transformsForZoom(_ type: JKHImageZoomType, toFrame: CGRect, fromFrame: CGRect) -> (from: CGAffineTransform, to: CGAffineTransform) {
+    fileprivate func animate(withTransitionContext context: UIViewControllerContextTransitioning?, animations: @escaping () -> Swift.Void, completion: ((Bool) -> Swift.Void)? = nil) {
+        
+        UIView.animate(withDuration: self.transitionDuration(using: context), delay: 0, usingSpringWithDamping: self.springDamping, initialSpringVelocity: self.springVelocity, options: .curveLinear, animations: animations, completion: completion)
+
+    }
+    
+    fileprivate func transformsForZoom(withType type: JKHImageZoomType, toFrame: CGRect, fromFrame: CGRect) -> (from: CGAffineTransform, to: CGAffineTransform) {
         let fromFrameAspectRatio = fromFrame.width / fromFrame.height
         let toFrameCenter = CGPoint(x: toFrame.midX, y: toFrame.midY)
         let fromFrameCenter = CGPoint(x: fromFrame.midX, y: fromFrame.midY)
@@ -129,22 +132,42 @@ class JKHImageZoomAnimationController: NSObject, UIViewControllerAnimatedTransit
         // From Transform
         let transScaleX = (toFrame.size.height * fromFrameAspectRatio) / fromFrame.size.width
         let transScaleY = (toFrame.size.height) / fromFrame.size.height
-        let fromTransPanY = (toFrameCenter.y - fromFrameCenter.y) / transScaleY
-        let fromTransPanX = (toFrameCenter.x - fromFrameCenter.x) / transScaleX
+        let fromTransPanY = toFrameCenter.y - fromFrameCenter.y
+        let fromTransPanX = toFrameCenter.x - fromFrameCenter.x
         
         let fromTransScale = CGAffineTransform(scaleX: transScaleX, y: transScaleY)
         let fromTransPan = CGAffineTransform(translationX: fromTransPanX, y: fromTransPanY)
-        let fromTransform = fromTransPan.concatenating(fromTransScale)
+        let fromTransform = fromTransScale.concatenating(fromTransPan)
         
         // To Transform
-        let toTransPanY = (fromFrameCenter.y - toFrameCenter.y) * (type == .inward ? transScaleY : 1)
-        let toTransPanX = (fromFrameCenter.x - toFrameCenter.x) * (type == .inward ? transScaleX : 1)
+        let toTransPanY = fromFrameCenter.y - toFrameCenter.y
+        let toTransPanX = fromFrameCenter.x - toFrameCenter.x
     
         let toTransScale = CGAffineTransform(scaleX: 1 / transScaleX, y: 1 / transScaleY)
         let toTransPan = CGAffineTransform(translationX: toTransPanX, y: toTransPanY)
-        let toTransform = toTransPan.concatenating(toTransScale)
-        
+        let toTransform = toTransScale.concatenating(toTransPan)
+    
         return (fromTransform, toTransform)
+    }
+    
+    fileprivate func setAnchorPoint(_ anchorPoint:CGPoint, forView view:UIView) {
+        var newPoint = CGPoint(x:view.bounds.size.width * anchorPoint.x, y:view.bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPoint(x:view.bounds.size.width * view.layer.anchorPoint.x, y:view.bounds.size.height * view.layer.anchorPoint.y)
+        
+        newPoint = newPoint.applying(view.transform)
+        oldPoint = oldPoint.applying(view.transform)
+        
+        var position = view.layer.position
+        
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        view.layer.position = position
+        view.layer.anchorPoint = anchorPoint
+    
     }
     
 }
